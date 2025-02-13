af80d00ede9986dc99770b4524f2fff00280c80e
louis-ngugi <louisngugi858@gmail.com>
2025-02-14 01:57:46 +0300

    updated conditional statement in lib/looping.py

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,30 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+    
+happy_new_year()
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    return[x ** 2 for x in int_list]
+
+print(square_integers([1, 2, 3, 4, 5]))
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        
+fizzbuzz()
